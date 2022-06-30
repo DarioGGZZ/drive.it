c1dc67492df6514d6c0fe3a04eda9aa8af1347f7
TMG <38388310+DarioGGZZ@users.noreply.github.com>
2022-06-29 22:53:22 -0300

    first commit

---
+++ src/main/java/com/dario/drive/it/Application.java
@@ -0,0 +1,13 @@
+package com.dario.drive.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
